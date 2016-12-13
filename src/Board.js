5b82daba9c178da4bfb686857e1f2a41d4b645f7
Alexandru Piticas <piticas.alexandru@gmail.com>
2016-12-13 11:15:06 +0100

    Storing a History

--- src/Board.js
+++ src/Board.js
@@ -1,50 +1,15 @@
 import React, { Component } from 'react';
-import calculateWinner from './Utils.js';
 import Square from './Square.js';
 
 class Board extends Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-
-        if (squares[i] || calculateWinner(squares)) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+        return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
