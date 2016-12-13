655cf3bb1770704b3a1c01e2264ca9756c53a015
Alexandru Piticas <piticas.alexandru@gmail.com>
2016-12-13 11:46:33 +0100

    Implementing Time Travel

--- src/Game.js
+++ src/Game.js
@@ -8,6 +8,7 @@ class Game extends Component {
     super();
 
     this.state = {
+      stepNumber: 0,
       history: [
         { squares: Array(9).fill(null) }
       ],
@@ -16,7 +17,7 @@ class Game extends Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -27,14 +28,22 @@ class Game extends Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
     this.setState({
+      stepNumber: history.length,
       history: history.concat([{ squares: squares }]),
       xIsNext: !this.state.xIsNext
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false :true
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     let status;
@@ -44,6 +53,16 @@ class Game extends Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Game start';
+
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
         <div className="game-board">
@@ -51,7 +70,7 @@ class Game extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
